ed0b59d5f697c7d6d9d47cb97558d6077555139f
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-10-29 13:48:34 +0700

    Connect to MongoDB

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,20 @@
 import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const {MONGODB_ATLAS_USERNAME,MONGODB_ATLAS_PASSWORD,MONGODB_ATLAS_DBNAME} = process.env
+
+const URL = `mongodb+srv://celvine:${MONGODB_ATLAS_PASSWORD}@cluster0.rhwyt.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`
+const OPTIONS = {
+    userNewUrlParser: true,
+    useUnifiedTopology : true
+}
+
 
 const APP = express();
 const PORT = 8080;
+
+APP.use(cors())
 
 APP.get("/", (req:Request, res:Response) => {
     res.send("HELLO, WORLD")
@@ -11,6 +24,12 @@ APP.get("/about",(req:Request, res:Response)=>{
     res.send("Hello This Route About")
 })
 
-APP.listen(PORT,()=>{
-    console.info(`App Listening in https://localhost:${PORT}`);
+mongoose.set('useFindAndModify', true)
+mongoose.connect(URL, OPTIONS).then(()=>{
+    APP.listen(PORT, ()=>{
+        console.info(`App Listening in https://localhost:${PORT}`);
+    });
+})
+.catch(error=>{
+    throw error
 })
