414759561ede88f1f9577a6c166950f51cb1e7ef
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-10-30 18:32:18 +0700

    create CRUD step 2

--- server/src/controllers/todos/index.ts
+++ server/src/controllers/todos/index.ts
@@ -61,3 +61,29 @@ export const updateTodo = async(req: Request, res: Response): Promise<void> =>{
         todos:updatedAllTodosAfterUpdate
     })
 }
+export const removeTodo = async(req: Request, res: Response): Promise<void> =>{
+    const {params: {id}} = req
+    if(!id){
+        res.status(401).json({
+            status: 401,
+            errorMessage : 'Validation Error: params _id is not defined'
+        });
+        return
+    }
+    const removeTodo = await TodoModel.findByIdAndRemove(id)
+    const updatedAllTodosAfterRemove = await TodoModel.find()
+
+    if(!removeTodo){
+        res.status(501).json({
+            status: 501,
+            errorMessage: 'Remove todo failed, Not Implement'
+        })
+        return
+    }
+
+    res.status(200).json({
+        message : 'Todo Successfully removed',
+        removeTodo,
+        todos: updatedAllTodosAfterRemove
+    })
+}
