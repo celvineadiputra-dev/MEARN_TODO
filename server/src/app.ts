f99b4ee7b67f514b9260f8d69287ef9c9b76b11a
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-11-01 18:55:50 +0700

    testing CRUD OKE

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,28 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import routes from './routes'
 
-const {MONGODB_ATLAS_USERNAME,MONGODB_ATLAS_PASSWORD,MONGODB_ATLAS_DBNAME} = process.env
+const {MONGODB_ATLAS_USERNAME,MONGODB_ATLAS_PASSWORD,MONGODB_ATLAS_DBNAME} = process.env;
 
-const URL = `mongodb+srv://celvine:${MONGODB_ATLAS_PASSWORD}@cluster0.rhwyt.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`
+const URL = `mongodb+srv://celvine:${MONGODB_ATLAS_PASSWORD}@cluster0.rhwyt.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`;
 const OPTIONS = {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology : true
 }
 
 const APP = express();
 const PORT = 8080;
 
-APP.use(cors())
+APP.use(cors());
+APP.use(routes);
 
-mongoose.set('useFindAndModify', true)
+mongoose.set('useFindAndModify', true);
 mongoose.connect(URL, OPTIONS).then(()=>{
     APP.listen(PORT, ()=>{
         console.info(`App Listening in https://localhost:${PORT}`);
     });
 })
-.catch(error=>{
+.catch((error)=>{
     throw error
 })
