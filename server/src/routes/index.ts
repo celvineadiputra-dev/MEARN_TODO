623711675d1a82f546c8d895f5e3dda4b905dd54
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-11-01 18:24:06 +0700

    add middleware bodyparser

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,14 @@
 import {Router} from 'express'
 import {getTodos, getTodo, addTodo, updateTodo, removeTodo} from "../controllers/todos"
+import bodyParser from 'body-parser'
+
 const ROUTER = Router()
+const JSONPARSER = bodyParser.json()
 
 ROUTER.get('/api/todos', getTodos)
 ROUTER.get('/api/todo/:id', getTodo)
-ROUTER.post('/api/add-todo', addTodo)
-ROUTER.put('/api/update-todo/:id', updateTodo)
-ROUTER.delete('/api/remove-todo/:id', removeTodo)
+ROUTER.post('/api/add-todo', JSONPARSER, addTodo)
+ROUTER.put('/api/update-todo/:id',JSONPARSER, updateTodo)
+ROUTER.delete('/api/remove-todo/:id',JSONPARSER, removeTodo)
 
 export default ROUTER
